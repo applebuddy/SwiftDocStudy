11e671eb2e72f1c0557dd6c9c47522dbecd0e488
MinKyeongTae <kyeongtea92@gmail.com>
2020-07-31 19:34:23 +0900

    Study String Literals in Swift documentation

---
+++ SwiftDocStudy.playground/Pages/StringLiteral.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+
+// MARK: - String Literals
+
+// MARK: How to Handle String Literals
+
+let someString = "Some string Literal value 🤗"
+
+let tripleLineStringLiterals = """
+ 
+    Hello I'm MungGu
+    I'm fine Thank you and you?
+    Okey Bye ~ See you again~
+ 
+""" // 맨 끝의 """는 들여쓰기의 기준이 됩니다. 해당 """의 시작점이 들여쓰기 시작점이 됩니다.
+
+print(tripleLineStringLiterals)
